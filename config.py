6841aef2abae6f1dad02966c0f361ae3cfe94df1
USERBOT-SUPPORT <85322232+USERBOT-SUPPORT@users.noreply.github.com>
2021-07-10 15:35:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 que = {}
-SESSION_NAME = getenv("SESSION_NAME", "session")
+SESSION_NAME = getenv("SESSION_NAME", "AQBorcx_--uV89FY1mxJW_UO5DcJxn9WIVZ3tV44UjHWoquLa3t-x7ewT7XlBBP_ZCoK47-6IwZ5QMDkvt1spOl_1TFR2VxgFrjslZz8iRk007RU09Lc_v-MhlB9omZDJiCAnxhzz4EBg3H5_Eb5D9JJWjLRcPQ_CN7nhmFsAPn-AJfY6vDMirKz3pUq4HHjOm6DDUrXVW_PTf4fwlSD_XFVVEIPur90GPgA0VqBI3Dgbrt3cd2BvkHvpHrHsw55Sh4t3uEuZ9A9ibQEUU1LfZejYPs7zsrxeaIhfLS9W37cW73k7LMpFA0yo4_QKTikgfss5Dofcb2UvjyuwhBuUJZ0b5MHgAA")
 BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_NAME = getenv("BOT_NAME")
 admins = {}
