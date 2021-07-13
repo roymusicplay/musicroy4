8c03612af56398d99b45a0b1e77284bea4711273
HITMAN | KARTIK <73695869+kartikrajofficial@users.noreply.github.com>
2021-07-13 18:43:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,13 +5,13 @@ from dotenv import load_dotenv
 load_dotenv()
 que = {}
 SESSION_NAME = getenv("SESSION_NAME", "AQBorcx_--uV89FY1mxJW_UO5DcJxn9WIVZ3tV44UjHWoquLa3t-x7ewT7XlBBP_ZCoK47-6IwZ5QMDkvt1spOl_1TFR2VxgFrjslZz8iRk007RU09Lc_v-MhlB9omZDJiCAnxhzz4EBg3H5_Eb5D9JJWjLRcPQ_CN7nhmFsAPn-AJfY6vDMirKz3pUq4HHjOm6DDUrXVW_PTf4fwlSD_XFVVEIPur90GPgA0VqBI3Dgbrt3cd2BvkHvpHrHsw55Sh4t3uEuZ9A9ibQEUU1LfZejYPs7zsrxeaIhfLS9W37cW73k7LMpFA0yo4_QKTikgfss5Dofcb2UvjyuwhBuUJZ0b5MHgAA")
-BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_NAME = getenv("BOT_NAME")
+BOT_TOKEN = getenv("1807956602:AAH9qrMyyf2mo3kJh2jeodvDhoJkwsw_pUM")
+BOT_NAME = getenv("⭐ KINGBOT MUSIC BOT⭐")
 admins = {}
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("4810374"))
+API_HASH = getenv("6aafb0eab6a7f0fdedc79df7a17efb98")
 
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
 
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 
